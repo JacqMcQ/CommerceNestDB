af0d889d8870b69afc8c93ea3edab4ad111994c2
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-08-08 09:09:33 -0600

    updated server.js file to import sequlize and models as well as error handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { sequelize } = require('./models')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+
+sequelize.sync({ alter: true })
+.then(() => {
+  console.log('\n----- DATABASE SYNCED -----\n');
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
+})
+
+.catch(err => {
+  console.error('Error connecting to the database:', err);
+  process.exit(1);
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Not Found',
+  });
 });
